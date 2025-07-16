8c0969676fbe134285f64fb5016f9bda53de0104
PranayDeep <pranaydeep921@gmail.com>
2025-07-16 12:41:11 +0530

    Fix MongoDB Atlas connection

--- habit-tracker-backend/server.js
+++ habit-tracker-backend/server.js
@@ -13,10 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
