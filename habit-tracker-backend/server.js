f5b3b3522e9179edb5ce2180d9e3d0647afd950d
PranayDeep <pranaydeep921@gmail.com>
2025-07-16 12:23:35 +0530

    Fix Express v5 compatibility issue

--- habit-tracker-backend/server.js
+++ habit-tracker-backend/server.js
@@ -31,7 +31,8 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
